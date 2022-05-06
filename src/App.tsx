e08eac1b49dee0b5e52b3a8f2bc790f4240383b3
Paul Gaspardo <paulgaspardo@outlook.com>
2022-05-06 14:31:50 +0200

    Update and tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -23,12 +23,12 @@ function App() {
   useEffect(() => {
     ModalService.getRequest().then(req => {
       if (req) {
-        let match = req.matching.find(i => i.side === 'accepting' && i.form === 'content-download');
+        let match = req.matching.find(i => i.side === 'accepting' && i.form === 'content-download' || i.form === 'content-blob');
         if (match) {
           setRequest(req);
-          setRequestStatus('All good - sending a content-download to ' + match.origin);
+          setRequestStatus('(A compatible request was detected, from ' + match.origin + ')');
         } else {
-          setRequestStatus('(This page is open in a poppy, but the client request is not compatible - this page is offering a content-download)');
+          setRequestStatus('(This page is open in a poppy, but the client request is not compatible - this page is offering a content-download or content-blob)');
         }
       } else {
         setRequestStatus('(It does not appear this page is open in a poppy - no request was detected)');
@@ -39,17 +39,16 @@ function App() {
   return (
     <div className='App'>
       <div className='App-header'>
-        <h1 className='App-headerItem'>Commons Picker</h1>
-        <form className='App-headerItem' onSubmit={ev => ev.preventDefault()}>
-          <label htmlFor="searchInput">Search: </label>
+        <form className='App-headerItem' onSubmit={ev => { ev.preventDefault(); document.getElementById('searchInput')?.blur(); }} style={{float: 'left'}}>
+          <label htmlFor="searchInput">Search <strong>Flickr Commons</strong>:</label><br />
           <input value={search} id="searchInput" autoFocus type="text" onChange={updateSearch} />
         </form>
-        <div className='App-headerItem'>
-          <button onClick={() => ModalService.close()}>Cancel - Close Poppy</button>
+        <div className='App-headerItem' style={{float:'right'}}>
+          <button onClick={() => ModalService.close()}>Cancel</button>
         </div>
       </div>
-        {!!search ? <SearchResults search={search}/> : <Welcome requestStatus={requestStatus} setSearch={setSearch}/>}
-      </div>
+      {!!search ? <SearchResults search={search}/> : <Welcome requestStatus={requestStatus} setSearch={setSearch}/>}
+    </div>
   );
 }
 function Welcome(props: {requestStatus: string, setSearch: (search: string) => void}) {
@@ -67,9 +66,10 @@ function Welcome(props: {requestStatus: string, setSearch: (search: string) => v
       send it back to the client page that opened it. {props.requestStatus}
     </p>
     <p>
-      Type in a
-      search above to get started - if you don't have any ideas maybe
-      try <a href='#' onClick={pickMe}>Moon</a> or <a href='#' onClick={pickMe}>Rover</a> or <a href='#' onClick={pickMe}>Saturn</a>.
+      Type in a search above to get started - if you don't have any ideas maybe
+      try <a href='#pick-moon' onClick={pickMe}>Moon</a> {' '}
+      or <a href='#pick-rover' onClick={pickMe}>Rover</a> {' '}
+      or <a href='#pick-rocket' onClick={pickMe}>Rocket</a>.
     </p>
     <p>
       This service is not affiliated with Flickr or Smugmug, it just uses the Flickr
@@ -80,43 +80,109 @@ function Welcome(props: {requestStatus: string, setSearch: (search: string) => v
     </div>;
 }
 
+type Search = SearchStarting | SearchResults | SearchError;
+
+type SearchStarting = {
+  state: 'starting';
+  loading: true;
+  hasMore: true;
+}
+
+interface SearchResults {
+  loading: boolean;
+  state: 'results';
+  results: any[];
+  totalResults: number;
+  hasMore: boolean;
+  loadMore(): void;
+}
+
+type SearchError = {
+  state: 'error';
+  message: string;
+  loading: false;
+  hasMore: false;
+  retry: () => void;
+}
+
+function useSearch(query: string) {
+  let [search, setSearch] = useState<Search>({ state: 'starting', loading: true, hasMore: true });
+  useEffect(() => {
+    let currentSearch = true;
+    setSearch({ state: 'starting', loading: true, hasMore: true });
+    let loadPage = async (previousResults: any[], page: number) => {
+      try {
+        if (currentSearch) setSearch(s => {
+          if (s.state === 'results') {
+            return {
+              state: 'results',
+              loading: true,
+              totalResults: s.totalResults,
+              results: s.results,
+              hasMore: s.hasMore,
+              loadMore() {}
+            };
+          }
+          return s;
+        });
+        let res = await fetch('https://f4r.poppy.io/api/commons-search?c=25&p=' + page + '&q=' + encodeURIComponent(query));
+        if (res.status !== 200) {
+          throw new Error('HTTP ' + 200);
+        }
+        let { photos } = await res.json();
+        let results = [...previousResults, ...photos.photo];
+        let loadingMore = false;
+        if (currentSearch) setSearch({
+          state: 'results',
+          loading: false,
+          totalResults: photos.total,
+          results,
+          hasMore: photos.page <= photos.pages,
+          loadMore() {
+            if (loadingMore) return;
+            loadingMore = true;
+            loadPage(results, page + 1);
+          }
+        })
+      } catch (e: any) {
+        console.error(e);
+        if (currentSearch) setSearch({
+          state: 'error',
+          message: e && e.message || 'Error',
+          retry()  {
+            loadPage(previousResults, page);
+          },
+          loading: false,
+          hasMore: false
+        });
+      }
+    };
+    loadPage([], 1);
+    return () => { currentSearch = false; }
+  }, [query]);
+  return search;
+}
+
 function SearchResults(props: {search: string}) {
-  console.log(props.search);
-  let [page, setPage] = useState(1);
-  let [totalResults, setTotalResults] = useState(Number.MAX_SAFE_INTEGER);
-  let [results, setResults] = useState<any[]>([]);
-  let [loading, setLoading] = useState(false);
-  let [selectedImage, setSelectedImage] = useState<any>();
 
-  let onLoadMore = async (specificPage?: number) => {
-    setLoading(true);
-    console.log('Fetch ' + specificPage || page);
-    let res = await fetch('https://f4r.poppy.io/api/commons-search?c=25&p=' + (specificPage||page) + '&q=' + encodeURIComponent(props.search));
-    let { photos } = await res.json();
-    setTotalResults(photos.total);
-    setResults([...results, ...photos.photo]);
-    setPage(page + 1);
-    setLoading(false);
-  };
-
+  let search = useSearch(props.search);
   let [infiniteScrollRef] = useInfiniteScroll({
-    hasNextPage: results.length < totalResults,
-    loading,
-    onLoadMore
+    hasNextPage: search.hasMore,
+    loading: search.loading,
+    onLoadMore: () => { if ('loadMore' in search) search.loadMore() }
   });
 
-  useEffect(() => { 
-    setResults([]);
-    setPage(1);
-    onLoadMore()
-  }, [props.search]);
+  let [selectedImage, setSelectedImage] = useState<any>();
 
   return <>
     {selectedImage ? <SelectedImage selected={selectedImage} cancel={() => setSelectedImage(null)}/> : null}
-    <div style={{paddingTop: '3rem'}}>
-      {results.map((res, idx) => <SearchResult result={res} key={idx} onSelect={setSelectedImage}/>)}
-      <div ref={infiniteScrollRef}>
-        {loading ? 'Loading...' : ''}
+    <div style={{paddingTop: '5rem'}}>
+      {search.state === 'results' ? search.results.map((res, idx) => <SearchResult result={res} key={idx} onSelect={setSelectedImage}/>) : null}
+      <div ref={infiniteScrollRef} style={{textAlign: 'center', paddingBottom: '2em'}}>
+        {search.loading ? 'Loading...' :
+        search.state === 'error' ? ['Error: ' + search.message, <button onClick={search.retry}>Retry</button>] :
+        search.state === 'results' && !search.hasMore ? `That's all ${search.results.length} results` :
+        ''}
       </div>
     </div>
   </>;
@@ -148,7 +214,7 @@ function SelectedImage(props: {selected: any, cancel: () => void}) {
         <h2>
           {result.title}
         </h2>
-        <div style={{float: 'left', padding: '1rem'}}>
+        <div className='App-selectionBox-options'>
           <div>
             <img src={thumbnailSrc} alt={result.title}/>
           </div>
@@ -159,29 +225,48 @@ function SelectedImage(props: {selected: any, cancel: () => void}) {
             <a href='#back-to-results' onClick={ev => {ev.preventDefault(); props.cancel();}}>Back to Results</a>
           </p>
           <p>
-            <a href='#cancel-totally' onClick={ev => {ev.preventDefault(); ModalService.close(); }}>Cancel and Return to App</a>
+            <a href='#cancel-totally' onClick={ev => {ev.preventDefault(); ModalService.close(); }}>Cancel and Close Poppy</a>
           </p>
         </div>
-        <div>
-          <p>{details ? 'From ' + details.owner.username : 'Loading...'}</p>
+        <div className='App-selectionBox-description'>
+          <p>{details ? 'Source: ' + details.owner.username : 'Loading...'}</p>
           <p dangerouslySetInnerHTML={{__html:sanitizeHtml(details?.description?._content?.replace(/\n/g,'<br>') || '')}}/>
         </div>
+        
       </div>
     </div>
   </>
 }
 
 async function onSelect(selection: any) {
+  let download = 'https://farm' + selection.farm + '.static.flickr.com/' + selection.server + '/' + selection.id + '_' + selection.originalsecret + '_o.jpg';
+  let title = selection.title;
+  let description = selection.description._content;
+
   let req = await ModalService.getRequest();
   if (req) {
-    await req.open({
-      offering: 'content-download',
-      sending: {
-        download: 'https://farm' + selection.farm + '.static.flickr.com/' + selection.server + '/' + selection.id + '_' + selection.originalsecret + '_o.jpg',
-        title: selection.title,
-        description: selection.description._content,
+    await req.open([
+      {
+        offering: 'content-download',
+        sending: {
+          download,
+          title,
+          description
+        }
+      },
+      {
+        offering: 'content-blob',
+        sending: async () => {
+          let res = await fetch(download);
+          let blob = await res.blob();
+          return {
+            blob,
+            title,
+            description
+          };
+        }
       }
-    });
+    ]);
     ModalService.close();
   }
 }
